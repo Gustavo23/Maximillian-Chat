8bf1356e71f06086b4193b7bc966681a7dfa37a5
Gustavo <gustavo.loureiro@buser.com.br>
2020-06-11 20:51:52 -0300

    @basic authscreen layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maximilliam_chat/screens/auth_screen.dart';
 import 'package:maximilliam_chat/screens/chat_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }

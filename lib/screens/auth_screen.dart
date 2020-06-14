e14e5862f875d662a8e893cc10a41a05b61fe8ba
Gustavo <gustavo.loureiro@buser.com.br>
2020-06-13 21:22:48 -0300

    @implements authentication

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maximilliam_chat/widgets/auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -7,13 +9,44 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+
   void _submitAuthForm(
     String email,
     String username,
     String password,
     bool isLogin,
-  ) {
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } on PlatformException catch (err) {
+      String errMessage = 'An error ocurred, please check your credentials!';
 
+      if (err.message != null) {
+        errMessage = err.message;
+      }
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(errMessage),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
